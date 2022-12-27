53aee0a1f6a47cf3a9ebfff6bcbbe9f277004991
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-12-27 18:18:23 -0300

    feat: added routes to app main file

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const routes = require('./routes/index');
+const routes = require('./routes/index');
 
 const app = express();
 
@@ -8,6 +8,6 @@ app.use(cors());
 
 app.use(express.json());
 
-// app.use(routes);
+app.use(routes);
 
 module.exports = app;
