ef06e11643193c11f3aa4e9e963a8b44caad457f
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-12-27 18:41:54 -0300

    feat: added error treatment on OrderController

--- back-end/src/controllers/order.controller.js
+++ back-end/src/controllers/order.controller.js
@@ -1,9 +1,13 @@
 const OrderService = require('../services/order.service');
 
 const getAll = async (_req, res) => {
-  const ordersInformation = await OrderService.getAll();
+  try {
+    const ordersInformation = await OrderService.getAll();
 
-  res.status(200).json(ordersInformation);
+    res.status(200).json(ordersInformation);
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
 
 module.exports = {
