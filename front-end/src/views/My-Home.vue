e6949020d86a7742c1ea8be8c45eb2e23f55b585
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-12-29 20:13:11 -0300

    feat: created Home view, and added sidebar

---
+++ front-end/src/views/My-Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-row">
+    <!-- NAV BAR -->
+    <div class="h-screen w-4/12  bg-slate-100">
+      <side-bar />
+    </div>
+    <!-- CONTENT -->
+    <div>
+      <!-- HEADER -->
+      <div></div>
+      <!-- TABLE -->
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from '../components/My-SideBar.vue'
+
+export default {
+  components: {
+    'side-bar': SideBar,
+  }
+}
+</script>
