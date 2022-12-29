ae2185534490d56091e3aa1f90fcf0b05de9d85b
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-12-29 19:58:07 -0300

    feat: added side bar component

---
+++ front-end/src/components/My-SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img src="../assets/images/cashforcelogo.svg" alt="cashforce logo" class=" ml-4 my-6" />
+    <div class="flex flex-col py-4 pl-4 border-l-4 border-cf-green">
+      <div class="flex">
+        <img class="mr-2" src="../assets/images/menu-item.svg" alt="icone do menu">
+        <p class="text-sm text-cf-green ">Notas fiscais</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
