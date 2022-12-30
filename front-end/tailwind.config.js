737ce50f9702bc9a3db205d417738a7c184a8363
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-12-30 00:23:55 -0300

    feat: added table component with Api information

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         'cf-green': '#00AD8C',
+        'cf-dark-blue': '#021B51',
+        'cf-dark-grey': '#727D94;',
+        'cf-mid-grey': '#A1A8B8',
+        'cf-dark2-grey': '#4D5566',
       }
     },
   },
